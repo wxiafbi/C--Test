58dd72a1af9a53c08680579adddb0ba8fff312d4
wxiafbi <youhappy-soihappy@qq.com>
2022-11-28 02:01:54 +0800

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: F:/Qt_Project/C++Test
+# Build directory: F:/Qt_Project/C++Test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
